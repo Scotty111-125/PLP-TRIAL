ba5258de39020eb0ee807e908050794da078a15f
Scotty <openda2002@gmail.com>
2025-07-30 20:27:02 +0300

    Introduction to python assignment

---
+++ ASSIGNMENT.py
@@ -0,0 +1,33 @@
+A=float(input("enter first number :"))
+B=float(input("enter the second number :"))
+
+#float enables the user to be able to use numbers with decimal points
+#input enables the user to input any number they desire
+
+# float is a data type
+#input() s a function
+
+#calculate sum
+SUM= A + B
+
+#generate out come
+#f is a string use in formarting
+print(f"{A} +{B} ={SUM}")
+
+#calculate difference
+DIFF= A - B
+
+#generate outcome
+print(f"{A} - {B} ={DIFF}")
+
+#calculate multiplication
+PRODUCT= A * B
+
+#generate outcome
+print(f"{A} * {B} ={PRODUCT}")
+
+#calculate division
+DIV= A / B
+
+#generate outcome
+print(f"{A} / {B} ={DIFF}")
